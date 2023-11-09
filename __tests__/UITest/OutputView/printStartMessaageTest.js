61ba1d74630ac44e4f82053da726b9b1987720dd
ss0526100 <ss0526100@gmail.com>
2023-11-09 22:13:01 +0900

    test: check printWelcomeMessage()

---
+++ __tests__/UITest/OutputView/printStartMessaageTest.js
@@ -0,0 +1,23 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import OutputView from '../../../src/controllers/OutputView';
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+
+  return logSpy;
+};
+
+test('printStartMessage()', () => {
+  //given
+  const outputView = OutputView;
+  const logSpy = getLogSpy();
+
+  //when
+  outputView.printStartMessaage();
+
+  //then
+  expect(logSpy.mock.calls[0][0]).toBe(
+    `안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.`
+  );
+});
