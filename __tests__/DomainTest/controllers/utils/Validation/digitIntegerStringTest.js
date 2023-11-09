7c52f69efa6d6bec52c5188490bffcc254e87791
ss0526100 <ss0526100@gmail.com>
2023-11-09 23:29:04 +0900

    test: fix wrong argument

--- __tests__/DomainTest/controllers/utils/Validation/digitIntegerStringTest.js
+++ __tests__/DomainTest/controllers/utils/Validation/digitIntegerStringTest.js
@@ -1,16 +1,28 @@
 import Validation from '../../../../../src/controllers/utils/Validation';
 
 describe('digitIntegerString()', () => {
-  test.each([['1', '13', '234213412341234', '-12', '0']])('정상작동', input => {
-    //given
-    const testFucntion = () => Validation.digitIntegerString(input);
+  test.each([['1'], ['13'], ['234213412341234'], ['-12'], ['0']])(
+    '정상작동',
+    input => {
+      //given
+      const testFucntion = () => Validation.digitIntegerString(input);
 
-    //when
-    expect(testFucntion).not.toThrow();
-  });
+      //when
+      expect(testFucntion).not.toThrow();
+    }
+  );
 
   test.each([
-    ['1.1', '', '-12341234.234', '0x123', '0b11', '0o11', '2e1', '1.1'],
+    [
+      ['1.1'],
+      [''],
+      ['-12341234.234'],
+      ['0x123'],
+      ['0b11'],
+      ['0o11'],
+      ['2e1'],
+      ['1.1'],
+    ],
   ])('예외', input => {
     //given
     const testFucntion = () => Validation.digitIntegerString(input);
