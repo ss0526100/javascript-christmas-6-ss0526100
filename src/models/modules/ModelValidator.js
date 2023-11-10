9921d2d5b62e04c5e2e3f49ca562debac142dc85
ss0526100 <ss0526100@gmail.com>
2023-11-10 20:32:55 +0900

    test(ModelValidator): check date in month

--- src/models/modules/ModelValidator.js
+++ src/models/modules/ModelValidator.js
@@ -5,8 +5,8 @@ import CONSTANT from '../../constants/CONSTANT.js';
 const { INVALID_DATE_ERROR_MESSAGE } = CONSTANT;
 
 const ModelValidator = Object.freeze({
-  dateInMonth(date, month) {
-    if (Calculator.lastDayByMonth(month) < date)
+  dateInMonth(date, month, leapYear = false) {
+    if (Calculator.lastDayByMonth(month, leapYear) < date)
       throw new Error(INVALID_DATE_ERROR_MESSAGE);
   },
 });
