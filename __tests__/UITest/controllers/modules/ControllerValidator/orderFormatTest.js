064b714c0d0b9f345aef026f2a34738782d8a675
ss0526100 <ss0526100@gmail.com>
2023-11-11 00:53:37 +0900

    test: check order format validation

---
+++ __tests__/UITest/controllers/modules/ControllerValidator/orderFormatTest.js
@@ -0,0 +1,23 @@
+import ControllerValidator from '../../../../../src/controllers/modules/ControllerValidator';
+
+describe('orderFormat()', () => {
+  test.each([['ckck- 1'], ['양송이수프-1'], ['초코케이크- 10'], ['20-10']])(
+    '정상동작',
+    input => {
+      //given
+      const testFunction = () => ControllerValidator.orderFormat(input);
+
+      expect(testFunction).not.toThrow();
+    }
+  );
+
+  test.each([['양송이수프'], ['양송이수프10개'], ['---0'], ['양송이수프--1']])(
+    '예외',
+    input => {
+      //given
+      const testFunction = () => ControllerValidator.orderFormat(input);
+
+      expect(testFunction).toThrow('[ERROR] 유효하지 않은 주문입니다.');
+    }
+  );
+});
--- src/constants/REGEXP.js
+++ src/constants/REGEXP.js
@@ -1,5 +1,8 @@
 const REGEXP = Object.freeze({
   DIGIT_INTEGER_REGULAR_EXPRESSION: Object.freeze(/^-?\d+$/),
+  ORDER_FORMAT_REGULAR_EXPRESSION: Object.freeze(
+    /^[가-힣|x-z|X-Z|1-9]{0,30},\s?\d+\s?$/
+  ),
 });
 
 export default REGEXP;
