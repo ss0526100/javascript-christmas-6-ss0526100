2e87732d868d9464688d223ecf694932dfc04a32
ss0526100 <ss0526100@gmail.com>
2023-11-12 03:23:21 +0900

    feat: check item count

--- src/models/modules/Ordervalidator.js
+++ src/models/modules/Ordervalidator.js
@@ -3,9 +3,8 @@ import CONSTANT from '../../constants/CONSTANT.js';
 const { INVALID_ORDER_ERROR_MESSAGE } = CONSTANT;
 
 const OrderValidator = Object.freeze({
-  itemInMenu(item, Menu) {
-    if (Menu.get(item.name) === undefined)
-      throw new Error(INVALID_ORDER_ERROR_MESSAGE);
+  itemCount(item) {
+    if (item.count < 1) throw new Error(INVALID_ORDER_ERROR_MESSAGE);
   },
 });
 
