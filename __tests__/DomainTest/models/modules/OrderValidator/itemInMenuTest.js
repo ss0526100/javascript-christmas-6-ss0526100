279216f8f3c123236d99980f3b4c518a79f3ed74
ss0526100 <ss0526100@gmail.com>
2023-11-12 02:29:57 +0900

    test(Order): check item in menu

---
+++ __tests__/DomainTest/models/modules/OrderValidator/itemInMenuTest.js
@@ -0,0 +1,53 @@
+import Ordervalidator from '../../../../../src/models/modules/OrderValidator';
+
+const mockMenu = menuArray => {
+  const menu = new Map();
+  menuArray.forEach(menu => {
+    const { name } = menu;
+    menu.set(null, { name });
+  });
+  return menu;
+};
+
+describe('itemInMenu()', () => {
+  test.each([
+    [
+      [{ name: '소불고기' }, { name: '돼지불고기' }, { name: '초벌삼겹살' }],
+      [
+        { name: '소불고기' },
+        { name: '돼지불고기' },
+        { name: '초벌삼겹살' },
+        { name: '기타메뉴' },
+      ],
+    ],
+    [
+      [{ name: '김치찌개' }],
+      [{ name: '된장찌개' }, { name: '고기찌개' }, { name: '김치찌개' }],
+    ],
+  ])('정상작동', (item, menuArray) => {
+    //given
+    const menu = mockMenu(menuArray);
+    const testFucntion = () => Ordervalidator.itemInMenu(item, menu);
+
+    //when
+    expect(testFucntion).not.toThrow();
+  });
+
+  test.each([
+    [
+      [{ name: '김치찌개' }],
+      [{ name: '된장찌개' }, { name: '고기찌개' }, { name: '짜치계' }],
+    ],
+    [
+      [{ name: '라면사리' }],
+      [{ name: '닭가슴살' }, { name: '브로콜리' }, { name: '불닭소스' }],
+    ],
+  ])('예외', (item, menuArray) => {
+    //given
+    const menu = mockMenu(menuArray);
+    const testFucntion = () => Ordervalidator.itemInMenu(item, menu);
+
+    //when
+    expect(testFucntion).toThrow('[ERROR] 유효하지 않은 주문입니다.');
+  });
+});
