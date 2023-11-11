7a5b131179ee707414cf21da30b084e82a913eac
ss0526100 <ss0526100@gmail.com>
2023-11-12 03:18:52 +0900

    test: check item count

---
+++ __tests__/DomainTest/models/modules/OrderValidator/itemCountTest.js
@@ -0,0 +1,22 @@
+import Ordervalidator from '../../../../../src/models/modules/OrderValidator';
+
+describe('itemCount()', () => {
+  test.each([[{ count: 10 }], [{ count: 2 }], [{ count: 5 }], [{ count: 7 }]])(
+    '정상작동',
+    item => {
+      //given
+      const testFucntion = () => Ordervalidator.itemCount(item);
+
+      //when
+      expect(testFucntion).not.toThrow();
+    }
+  );
+
+  test.each([[{ count: 0 }]])('예외', item => {
+    //given
+    const testFucntion = () => Ordervalidator.itemCount(item);
+
+    //when
+    expect(testFucntion).toThrow('[ERROR] 유효하지 않은 주문입니다.');
+  });
+});
