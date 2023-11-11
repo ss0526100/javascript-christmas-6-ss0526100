fe03cc9c532af87537cc551d042b9775f706abb2
ss0526100 <ss0526100@gmail.com>
2023-11-11 20:05:54 +0900

    test: check filterOrderItems

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -2,11 +2,14 @@ import { Console } from '@woowacourse/mission-utils';
 
 import CONSTANT from '../constants/CONSTANT.js';
 
-const { DATE_INPUT_MESSAGE } = CONSTANT;
+const { DATE_INPUT_MESSAGE, ORDER_ITEMS_INPUT_MESSAGE } = CONSTANT;
 
 const InputView = Object.freeze({
   async readDate() {
     return await Console.readLineAsync(DATE_INPUT_MESSAGE);
+  },
+  async readOrderItems() {
+    return await Console.readLineAsync(ORDER_ITEMS_INPUT_MESSAGE);
   },
   // ...
 });
