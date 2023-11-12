c45a1eb214224e80741ead3282b61f78c667fbec
ss0526100 <ss0526100@gmail.com>
2023-11-12 15:15:10 +0900

    test(Order): check same name in items

---
+++ __tests__/DomainTest/models/modules/OrderValidator/sameNameInItemTest.js
@@ -0,0 +1,26 @@
+import Ordervalidator from '../../../../../src/models/modules/OrderValidator';
+
+describe('sameNameInItems()', () => {
+  test.each([
+    [{ name: '케이크' }, { name: '쿠키' }, { name: '커피' }],
+    [{ name: '김치' }, { name: '소고기' }, { name: '돼지고기' }],
+    [{ name: '김치' }],
+  ])('정상작동', items => {
+    //given
+    const testFucntion = () => Ordervalidator.sameNameInItems(items);
+
+    //when
+    expect(testFucntion).not.toThrow();
+  });
+
+  test.each([
+    [{ name: '케이크' }, { name: '쿠키' }, { name: '쿠키' }],
+    [{ name: '소고기' }, { name: '소고기' }],
+  ])('예외', items => {
+    //given
+    const testFucntion = () => Ordervalidator.sameNameInItems(items);
+
+    //when
+    expect(testFucntion).toThrow('[ERROR] 유효하지 않은 주문입니다.');
+  });
+});
