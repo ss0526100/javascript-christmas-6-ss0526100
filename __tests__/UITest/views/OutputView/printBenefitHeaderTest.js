cdd5044d686d4c3d84a51d6d6220d8041eb8df67
ss0526100 <ss0526100@gmail.com>
2023-11-12 20:12:49 +0900

    test: check print benefit header

---
+++ __tests__/UITest/views/OutputView/printBenefitHeaderTest.js
@@ -0,0 +1,28 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import OutputView from '../../../../src/views/OutputView';
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+
+  return logSpy;
+};
+
+test.each([
+  [1, 21],
+  [4, 30],
+  [10, 30],
+  [3, 4],
+])('printBenefitHeader()', (month, day) => {
+  //given
+  const outputView = OutputView;
+  const logSpy = getLogSpy();
+
+  //when
+  outputView.printBenefitHeader(input);
+
+  //then
+  expect(logSpy.mock.calls[0][0]).toBe(
+    `${month}월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`
+  );
+});
