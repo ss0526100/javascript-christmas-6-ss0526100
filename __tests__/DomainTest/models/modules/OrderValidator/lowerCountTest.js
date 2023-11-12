b4d1547c2da86aa63a5329e30f9c33ad9cf68742
ss0526100 <ss0526100@gmail.com>
2023-11-12 15:33:30 +0900

    refactor: renma itemCount

--- __tests__/DomainTest/models/modules/OrderValidator/lowerCountTest.js
+++ __tests__/DomainTest/models/modules/OrderValidator/lowerCountTest.js
@@ -1,11 +1,11 @@
 import Ordervalidator from '../../../../../src/models/modules/OrderValidator';
 
-describe('itemCount()', () => {
+describe('lowerCount()', () => {
   test.each([[{ count: 10 }], [{ count: 2 }], [{ count: 5 }], [{ count: 7 }]])(
     '정상작동',
     item => {
       //given
-      const testFucntion = () => Ordervalidator.itemCount(item);
+      const testFucntion = () => Ordervalidator.lowerCount(item);
 
       //when
       expect(testFucntion).not.toThrow();
@@ -14,7 +14,7 @@ describe('itemCount()', () => {
 
   test.each([[{ count: 0 }]])('예외', item => {
     //given
-    const testFucntion = () => Ordervalidator.itemCount(item);
+    const testFucntion = () => Ordervalidator.lowerCount(item);
 
     //when
     expect(testFucntion).toThrow('[ERROR] 유효하지 않은 주문입니다.');
--- src/models/modules/Ordervalidator.js
+++ src/models/modules/Ordervalidator.js
@@ -3,7 +3,7 @@ import CONSTANT from '../../constants/CONSTANT.js';
 const { INVALID_ORDER_ERROR_MESSAGE } = CONSTANT;
 
 const OrderValidator = Object.freeze({
-  itemCount(item) {
+  lowerCount(item) {
     if (item.count < 1) throw new Error(INVALID_ORDER_ERROR_MESSAGE);
   },
   sameNameInItems(items) {
