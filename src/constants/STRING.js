1364cde5d6f1e053bb642b61fbb8a066a3b81794
ss0526100 <ss0526100@gmail.com>
2023-11-14 13:10:41 +0900

    chore: delete \n in string constant

--- src/constants/STRING.js
+++ src/constants/STRING.js
@@ -4,9 +4,9 @@ const STRING = Object.freeze({
   WELCOME_MESSAGE_HEADER: '안녕하세요! 우테코 식당 ',
   WELCOME_MESSAGE_FOOTER: '월 이벤트 플래너입니다.',
   DATE_INPUT_MESSAGE_FOOTER:
-    '월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
+    '월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
   ORDER_ITEMS_INPUT_MESSAGE:
-    '주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
+    '주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
   INPUT_RETRY_MESSAGE: '다시 입력해 주세요.',
   ALL_BENEFIT_MESSAGE_DIV: '월 ',
   ALL_BENEFIT_MESSAGE_FOOTER:
@@ -54,6 +54,7 @@ const STRING = Object.freeze({
   DASH: '-',
   BLANK: '',
   COLON: ':',
+  LINE_BREAK: '\n',
 });
 
 export default STRING;
